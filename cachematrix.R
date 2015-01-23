a2c176f5cbf37a77e74843e6f6b28722f2d2e5ab
rasportugal <ras_pt@yahoo.es>
2015-01-23 01:01:34 +0000

    cachematrix.R updated v02
    Solution and comments for assignmet 2

--- cachematrix.R
+++ cachematrix.R
@@ -10,23 +10,23 @@
 ## presented: the matrix itself and the inverse.
 
 makeCacheMatrix <- function(x = numeric()) {
-  
-  m <- NULL
-  
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  
-  get <- function() x
-  
-  setinverse <- function(inverse) m <<- inverse
-  
-  getinverse <- function() m
-  
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+        
+        m <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        get <- function() x
+        
+        setinverse <- function(inverse) m <<- inverse
+        
+        getinverse <- function() m
+        
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
@@ -37,20 +37,20 @@ makeCacheMatrix <- function(x = numeric()) {
 ## the cache.
 
 cacheSolve <- function(x, ...) {
-  m <- x$getinverse()
-  
-  ## when it is not the first time that the matrix is presented to cacheSove
-  ## it will consult the inverse matrix stored in the list (a.k.a. special 
-  ## "matrix")
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  
-  ## if it is the first time that the matrix is presented, it's effectively
-  ## computed the inverse matrix and it's stored in cache for future usage
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
+        m <- x$getinverse()
+        
+        ## when it is not the first time that the matrix is presented to cacheSove
+        ## it will consult the inverse matrix stored in the list (a.k.a. special 
+        ## "matrix")
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        ## if it is the first time that the matrix is presented, it's effectively
+        ## computed the inverse matrix and it's stored in cache for future usage
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
